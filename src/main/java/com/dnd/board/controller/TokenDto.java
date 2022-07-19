81d8eb9a650652fec5eb6112f8eebd27158d701a
tkfdkskarl56 <tkfdkskarl56@naver,com>
2022-07-19 23:38:22 +0900

    feat:Security JWT 구현

---
+++ src/main/java/com/dnd/board/controller/TokenDto.java
@@ -0,0 +1,13 @@
+package com.dnd.board.controller;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
---
+++ src/main/java/com/dnd/board/http/response/GeneralResponse.java
@@ -0,0 +1,16 @@
+package com.dnd.board.http.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class GeneralResponse {
+    private int code;
+    private String message;
+
+    public static GeneralResponse of(HttpStatus httpStatus, String message) {
+        return new GeneralResponse(httpStatus.value(), message);
+    }
+}
